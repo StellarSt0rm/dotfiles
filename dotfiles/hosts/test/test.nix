27a92936dcf3f72cecf69df9281ce88c5395d3f4
StellarSt0rm <StellarSt0rm@proton.me>
2025-01-18 15:50:01 +0100

    I forgot about the 'git aware' thingie, fuck. I'll just define the test host again for each test host. smh

--- dotfiles/hosts/test/test.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  imports = [ ./test-hardware.nix ];
-  
-  networking.hostName = "test";
-  system.stateVersion = "24.11";
-
-  home-manager.users.gemini.home.stateVersion = "24.11";
-}
