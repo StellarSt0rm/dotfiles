c21cc462d431d08d73b31301435cd715e401555f
StellarSt0rm <StellarSt0rm@proton.me>
2025-01-19 23:28:15 +0100

    Add zed as a flake, hopefully it works.

--- dotfiles/flake.nix
+++ dotfiles/flake.nix
@@ -11,6 +11,7 @@
     
     # Other Flakes
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zed-editor.url = "github:zed-industries/zed";
   };
   
   outputs = { self, nixpkgs, home-manager }@inputs:
