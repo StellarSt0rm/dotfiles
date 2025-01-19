3ef444c04c5b8c1f289414c934300eb3524a79ab
StellarSt0rm <stellarst0rm@proton.me>
2025-01-19 10:15:59 +0100

    Trash dir is stored as .trash folder in the selected dir, so i'll just put the same path.

--- dotfiles/modules/gnome/apps.nix
+++ dotfiles/modules/gnome/apps.nix
@@ -45,7 +45,7 @@
     
     "com/toolstack/Folio" = {
       notes-dir = "/home/gemini/Documents/Notes";
-      trash-dir = "/home/gemini/Documents/Notes/Trash";
+      trash-dir = "/home/gemini/Documents/Notes";
       enable-autosave = true;
       
       cheatsheet-enabled = true;
