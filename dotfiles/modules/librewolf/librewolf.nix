763769056870d88f1a40f4371fbf406eda3ac636
StellarSt0rm <StellarSt0rm@proton.me>
2025-01-25 16:27:17 +0100

    I prefer the gnome theme... Also some other lil changes to the librewolf config

--- dotfiles/modules/librewolf/librewolf.nix
+++ dotfiles/modules/librewolf/librewolf.nix
@@ -6,11 +6,11 @@
     };
   })];
   
-  home.file."material-fox" = {
-    target = ".librewolf/gemini/chrome/material-fox";
-    source = pkgs.fetchzip {
-      url = "https://github.com/edelvarden/material-fox-updated/releases/download/v1.3.0/chrome.zip";
-      hash = "sha256-axAKxcCnAw8ARmNHviuP/S8yAqj2KMCAfiz7r28Ymkw=";
+  home.file."gnome-theme" = {
+    target = ".librewolf/gemini/chrome/gnome-theme";
+    source = fetchTarball {
+      url = "https://github.com/rafaelmardojai/firefox-gnome-theme/archive/refs/tags/v134.tar.gz";
+      sha256 = "sha256:072kaq9x7gjzwxzql3yn4x523is65pgzczaaw9a2rdc4gnm4ggsb";
     };
   };
   
@@ -45,7 +45,15 @@
         
         engines = {
           "Startpage" = {
-            urls = [{ template = "https://startpage.com/do/search?query={searchTerms}&prfe=${builtins.readFile ./startpage.conf}"; }];
+            urls = [{
+              template = "https://startpage.com/do/search";
+              
+              params = [
+                { name = "query"; value = "{searchTerms}"; }
+                { name = "prfe"; value = (builtins.readFile ./startpage.conf); }
+              ];
+            }];
+            
             iconUpdateURL = "http://startpage.com/sp/cdn/favicons/favicon-gradient.ico";
           };
           
@@ -69,7 +77,10 @@
       ];
       
       settings = {
+        "privacy.clearOnShutdown_v2.cookiesAndStorage" = false;
+        "security.donottrackheader.enabled" = true;
         "browser.startup.page" = 3; # Keep open windows
+        "general.autoScroll" = true;
         
         # Annoyances
         "clipboard.autocopy" = false;
@@ -92,10 +103,12 @@
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "widget.gtk.rounded-bottom-corners.enabled" = true;
         "svg.context-properties.content.enabled" = true;
-        "layout.css.color-mix.enabled" = true;
+        "browser.theme.dark-private-windows" = false;
+        "browser.uidensity" = 0;
         
-        "userChrome.ui-context-menu-icons" = true;
-        "userChrome.theme-material" = false;
+        "gnomeTheme.systemIcons" = true;
+        "gnomeTheme.hideWebrtcIndicator" = true;
+        "gnomeTheme.hideSingleTab" = true;
         
         "browser.uiCustomization.state" = builtins.toJSON {
           placements = {
@@ -105,21 +118,18 @@
               "back-button"
               "forward-button"
               "stop-reload-button"
+              "new-tab-button"
               "customizableui-special-spring1"
               "urlbar-container"
               "customizableui-special-spring2"
               "save-to-pocket-button"
               "downloads-button"
-              "ublock0_raymondhill_net-browser-action"
               "fxa-toolbar-menu-button"
               "unified-extensions-button"
             ];
             toolbar-menubar = [ "menubar-items" ];
             
-            TabsToolBar = [
-              "tabbrowser-tabs"
-              "new-tab-button"
-            ];
+            TabsToolBar = [ "tabbrowser-tabs" ];
             PersonalToolbar = [ "import-button" "personal-bookmarks" ];
           };
           
@@ -139,13 +149,13 @@
       };
       
       userChrome = ''
-        @import "material-fox/userChrome.css";
+        @import "gnome-theme/userChrome.css";
         
         /* Custom styles */
         ${builtins.readFile ./styles/customChrome.css}
       '';
       userContent = ''
-        @import "material-fox/userContent.css";
+        @import "gnome-theme/userContent.css";
         
         /* Custom Content Styles */
         ${builtins.readFile ./styles/customContent.css}
