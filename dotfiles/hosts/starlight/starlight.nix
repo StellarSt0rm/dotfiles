d944c8b2f5d320068ef402763b10f1d1a89b422a
StellarSt0rm <StellarSt0rm@proton.me>
2025-02-09 19:07:56 +0100

    starlight: Add more packages

--- dotfiles/hosts/starlight/starlight.nix
+++ dotfiles/hosts/starlight/starlight.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [ ./starlight-hardware.nix ];
   
   networking.hostName = "starlight";
@@ -7,6 +7,18 @@
   home-manager.users.gemini.home.stateVersion = "24.11";
   programs.git.user.signingKey = "0CD71823FAFF0DAAEFAFEA6AE4607894190C2177";
 
-  # Install Steam
+  # Install extra packages
   programs.steam.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # Games
+    bottles
+    prismlauncher
+
+    # Tools
+    blender
+    gimp
+    godot_4
+    easytag
+  ];
 }
