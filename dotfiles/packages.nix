db663d604406972685122ccb9ec7dac825b9ab8c
StellarSt0rm <StellarSt0rm@proton.me>
2025-02-17 16:05:03 +0100

    Add nil and nixd, plus formatting.

--- dotfiles/packages.nix
+++ dotfiles/packages.nix
@@ -63,6 +63,9 @@
     warp
     xsel
 
+    nixd
+    nil
+
     # Themes
     papirus-icon-theme
     bibata-cursors
@@ -117,10 +120,11 @@
   };
 
   # Fix GStreamer Plug-in Issue
-  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
-    gst-plugins-good
-    gst-plugins-bad
-    gst-plugins-ugly
-    gst-libav
-  ]);
+  environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 =
+    lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" (with pkgs.gst_all_1; [
+      gst-plugins-good
+      gst-plugins-bad
+      gst-plugins-ugly
+      gst-libav
+    ]);
 }
