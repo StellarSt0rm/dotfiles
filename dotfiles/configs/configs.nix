3128d89869133b281e03b76c30c1ee631faa7e86
StellarSt0rm <stellarst0rm@proton.me>
2025-03-12 13:30:24 +0100

    Remove github cli settings

--- dotfiles/configs/configs.nix
+++ dotfiles/configs/configs.nix
@@ -7,13 +7,6 @@
     enable = true;
 
     config = {
-      "credential \"https://github.com\"" = {
-        helper = "!/run/current-system/sw/bin/gh auth git-credential";
-      };
-      "credential \"https://gist.github.com\"" = {
-        helper = "!/run/current-system/sw/bin/gh auth git-credential";
-      };
-
       user = {
         email = "StellarSt0rm@proton.me";
         name = "StellarSt0rm";
