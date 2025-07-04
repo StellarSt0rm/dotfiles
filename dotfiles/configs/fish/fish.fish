18d1c87ccf06c3a9219ed92c3d76f248145a7274
StellarSt0rm <StellarSt0rm@proton.me>
2025-07-04 13:36:12 +0200

    Move some stuff around

--- dotfiles/configs/fish/fish.fish
+++ dotfiles/configs/fish/fish.fish
@@ -105,12 +105,12 @@ alias clean-sys "sudo nix-collect-garbage -d && sudo /run/current-system/bin/swi
 bind \cq '__fish_sudo_ctrl_q'
 bind \cd 'echo -e "\033[4A"; exit' # Kinda fix the transient prompt not going away when using Ctrl + D
 
-stty -echo
 if test "$TERM_PROGRAM" != "zed" -a -z "$IN_NIX_SHELL"
-  clear
+  stty -echo
 
   echo -e " $(nixos-version) |  $(uname -r)\n"
   fortune -sn 80 -a riddles | sed 's/\t/ /g'
+
   echo
+  stty echo
 end
-stty echo
